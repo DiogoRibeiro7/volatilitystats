d6afe772841e4bfd2fe6ceb41f0ee486188e5f73
Diogo Ribeiro <diogo_dj@hotmail.com>
2025-04-22 14:09:45 +0100

    chore: develop

--- volstats/models/__init__.py
+++ volstats/models/__init__.py
@@ -5,6 +5,8 @@ from .egarch_model import estimate_egarch_params
 from .gjr_garch_model import estimate_gjr_garch_params
 from .stochastic_volatility_model import estimate_sv_params
 from .component_garch_model import estimate_component_garch_params
+from .garch_in_mean_model import estimate_garch_in_mean_params
+from .harch_model import estimate_harch_params
 
 __all__ = [
     "garch",
@@ -13,5 +15,7 @@ __all__ = [
     "estimate_egarch_params",
     "estimate_gjr_garch_params",
     "estimate_sv_params",
-    "estimate_component_garch_params"
+    "estimate_component_garch_params",
+    "estimate_garch_in_mean_params",
+    "estimate_harch_params",
 ]
